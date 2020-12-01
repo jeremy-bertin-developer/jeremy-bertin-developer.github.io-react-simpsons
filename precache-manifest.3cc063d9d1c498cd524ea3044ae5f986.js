e7c280e539fff309476f6235bb9ef1091d8e6ee3
Jeremy Bertin <jeremy.bertin76@hotmail.fr>
2020-12-01 21:49:35 +0100

    first commit

---
+++ precache-manifest.3cc063d9d1c498cd524ea3044ae5f986.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc7c313ecb282ab6186ee6cce858c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e39f2e6803e8d63f0b5d",
+    "url": "/static/css/main.2a4664b8.chunk.css"
+  },
+  {
+    "revision": "cb93634fd76b4cfae5b1",
+    "url": "/static/js/2.bcf703b1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bcf703b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e39f2e6803e8d63f0b5d",
+    "url": "/static/js/main.78610bde.chunk.js"
+  },
+  {
+    "revision": "3ffe26351d4f7e6d8497",
+    "url": "/static/js/runtime-main.07c9febd.js"
+  },
+  {
+    "revision": "42e79bc98d6499aa86c84979581b420d",
+    "url": "/static/media/Simpsonfont.42e79bc9.otf"
+  },
+  {
+    "revision": "183d915ba2dcbd947f4b7cc6db85b787",
+    "url": "/static/media/simpsons-bg-header.183d915b.png"
+  },
+  {
+    "revision": "05257f91ab00354ea6eb89f9e086122b",
+    "url": "/static/media/title.05257f91.png"
+  }
+]);
